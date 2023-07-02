99b1a251b35ce919f7efa08e419612bb1d1b3e5b
LAPTOP-QQBHK4EG\adoni <pknoc@protonmail.com>
2023-07-02 13:39:05 +0200

    Controllers Update

--- src/main/java/io/mkolodziejczyk92/anonymoussanta/data/controllers/UserController.java
+++ src/main/java/io/mkolodziejczyk92/anonymoussanta/data/controllers/UserController.java
@@ -1,9 +1,37 @@
 package io.mkolodziejczyk92.anonymoussanta.data.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.mkolodziejczyk92.anonymoussanta.data.model.UserDto;
+import io.mkolodziejczyk92.anonymoussanta.data.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public void createUser(@RequestBody UserDto userDto) {
+        UserDto savedUser = userService.saveUser(userDto);
+    }
+
+    @GetMapping
+    public List<UserDto> getAllUsers() {
+        return UserService.getAllUsers();
+    }
+
+    @PutMapping("/{id}")
+    public void updateUserById(@RequestBody UserDto userDto, @PathVariable Long id) {
+        userService.updateUserById(id, userDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUserById(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
 }
