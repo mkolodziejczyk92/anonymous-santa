bf59bfb5fc03c404de48416d637db5bdf5b9a3bf
LAPTOP-QQBHK4EG\adoni <pknoc@protonmail.com>
2023-07-02 13:40:14 +0200

    Dto Update

--- src/main/java/io/mkolodziejczyk92/anonymoussanta/data/model/UserDto.java
+++ src/main/java/io/mkolodziejczyk92/anonymoussanta/data/model/UserDto.java
@@ -1,4 +1,24 @@
 package io.mkolodziejczyk92.anonymoussanta.data.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.mkolodziejczyk92.anonymoussanta.data.enums.ERole;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
 public class UserDto {
+
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String registrationCode;
+    private Set<ERole> userRoles;
 }
