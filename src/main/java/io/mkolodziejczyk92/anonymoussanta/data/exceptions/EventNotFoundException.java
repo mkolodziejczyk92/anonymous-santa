878f939a35dd272790d6885ae713c7e9d8eca9d3
mkolodziejczyk92 <marcin.kdz92@gmail.com>
2023-09-20 23:09:50 +0200

    add exceptions

---
+++ src/main/java/io/mkolodziejczyk92/anonymoussanta/data/exceptions/EventNotFoundException.java
@@ -0,0 +1,7 @@
+package io.mkolodziejczyk92.anonymoussanta.data.exceptions;
+
+public class EventNotFoundException extends Exception {
+    public EventNotFoundException(String message) {
+        super(message);
+    }
+}
